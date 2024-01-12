efe41388efdf0d5f39511cebca18cb430e61f80d
Haroon Ul Hassan <93781661+haroonulhassan@users.noreply.github.com>
2024-01-13 02:56:43 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {},
+   env: {
+        MONGODB_URI:"mongodb+srv://haroonulhassan1:3770@cluster1.qgoqwti.mongodb.net/evantaa"
+      },
 
 module.exports = nextConfig
